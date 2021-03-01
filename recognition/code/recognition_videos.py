0d9fe2c81beabd274e511a2e8d8783323d36bd18
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-03-01 14:03:10 -0600

    finish traffic sign recognition code

--- recognition/code/recognition_videos.py
+++ recognition/code/recognition_videos.py
@@ -3,11 +3,8 @@ import numpy as np
 import pandas as pd
 import cv2
 import time
-# from timeit import default_timer as timer
 import matplotlib.pyplot as plt
 import pickle
-# from IPython.display import FileLink
-# import os
 from keras.models import load_model
 
 
@@ -19,111 +16,95 @@ def main(args):
     Output: None
     """
     # Reading csv file with labels' names
-    # Loading two columns [0, 1] into Pandas dataFrame
     labels = pd.read_csv("data/traffic-signs-preprocessed/label_names.csv")
     print("labels:\n", labels)
 
     ############################################################
 
     # Loading trained CNN model to use it later when classifying from 4 groups into one of 43 classes
-    model = load_model("data/model/model-5x5.h5")
+    model = load_model("model/model-5x5 (1).h5")
 
     # Loading mean image to use for preprocessing further
     with open("data/traffic-signs-preprocessed/mean_image_rgb.pickle", 'rb') as f:
-        mean = pickle.load(f, encoding='latin1')  # dictionary type
-
-    # print(mean["mean_image_rgb"].shape)  # (3, 32, 32)
+        mean = pickle.load(f, encoding='latin1')
 
     ###############################################################
 
     # Trained weights can be found in the course mentioned above
     model_path = "data/traffic-signs-dataset-in-yolo-format/signs.weights"
-    # model_markings_path = "data/traffic-signs-dataset-in-yolo-format/horizontal.weights"
     config_path = "data/traffic-signs-dataset-in-yolo-format/yolov3_ts_test.cfg"
-    # config_markings_path = "data/traffic-signs-dataset-in-yolo-format/markings_test.cfg"
 
     # Loading trained YOLO v3 weights and cfg configuration file by 'dnn' library from OpenCV
     net = cv2.dnn.readNetFromDarknet(config_path, model_path)
-    # net_markings = cv2.dnn.readNetFromDarknet(config_markings_path, model_markings_path)
 
-    # To use with GPU
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL_FP16)
+    # # Set the network to use with GPU
+    # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    # net.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL_FP16)
 
-    # net_markings.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-    # net_markings.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL_FP16)
+    # Set the network to use with CPU
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
     ##########################################################
 
     # Getting names of all YOLO v3 layers
     layers_all = net.getLayerNames()
     layers_names_output = [layers_all[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-    print(f"layers_names_output: {layers_names_output}")
-
-    # Getting names of all YOLO v4 layers
-    # layers_all_markings = net_markings.getLayerNames()
-    # layers_names_output_markings = [layers_all_markings[i[0] - 1] for i in net_markings.getUnconnectedOutLayers()]
-    # print(f"layers_names_output_markings: {layers_names_output_markings}")
+    # print(f"layers_names_output: {layers_names_output}")
 
     ############################################################
 
-    # Minimum probability to eliminate weak detections
-    probability_minimum = 0.05
+    # Setting confidence threshold
+    confidence_threshold = 0.06
 
-    # Setting threshold to filtering weak bounding boxes by non-maximum suppression
-    threshold = 0.08
+    # Setting non-maximum supression threshold
+    nms_threshold = 0.08
 
     # Generating colors for bounding boxes
     colors = np.random.randint(0, 255, size=(len(labels), 3), dtype='uint8')
-    # colors_markings = np.random.randint(0, 255, size=(1, 3), dtype='uint8')
-    # print("color: ", colors)
 
     #########################################################
 
     # Reading input video
-    # video = cv2.VideoCapture('input_video/70maiMiniDashCam-Dzien.mp4')
-    # video = cv2.VideoCapture('input_video/DODRX8W(lusterko)-roadtestwsonecznydzien_podsonce1080p30.mp4')
-    # video = cv2.VideoCapture('input_video/traffic-sign-to-test.mp4')
-    # video = cv2.VideoCapture('input_video/Video_Recognition.mp4')
     video = cv2.VideoCapture(args["video"])
 
     frame_tot = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     print(f"video length: {frame_tot} frames")
 
-    ########################################################
+    fps = video.get(cv2.CAP_PROP_FPS)
+    print(f"video fps: {fps}")
 
-    # Setting default size of plots
-    plt.rcParams["figure.figsize"] = (3, 3)
+    ########################################################
 
     # Initializing frame and processing time counters
     current_frame, current_time = 0, 0
 
-    # Capturing frames one-by-one
+    # Capturing the first frame
     ret, frame = video.read()
 
     # Finding frame size
     frame_height, frame_width = frame.shape[:2]
+    print(f"frame_height: {frame_height}\t frame_width: {frame_width}")
 
-    # Initializing output video
+    # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    output_video_name = "result6.mp4"
-    output_video = cv2.VideoWriter(output_video_name, fourcc, 25,
+    output_video_name = "result.mp4"
+    output_video = cv2.VideoWriter(output_video_name, fourcc, fps,
                              (frame_width, frame_height), True)
 
-    # Catching frames in the loop
-    while True:
+    # Processing the video frame-by-frame
+    while video.isOpened():
         start = time.time() 
 
         # Finding blob from current frame
+        # frame_resized = cv2.resize(frame, (224, 224))
         scalefactor = 1 / 255.0
         size = (416, 416)
         blob = cv2.dnn.blobFromImage(frame, scalefactor, size, swapRB=True, crop=False)
 
         # Forward pass with blob through output layers
         net.setInput(blob)
-        # net_markings.setInput(blob)
         net_output = net.forward(layers_names_output)
-        # net_markings_output = net_markings.forward(layers_names_output_markings)
 
         end = time.time()
         dt = end - start
@@ -138,11 +119,8 @@ def main(args):
 
         # Lists for detected bounding boxes, confidences and class's number
         bounding_boxes = []
-        # bounding_boxes_markings = []
         confidences = []
-        # confidences_markings = []
         class_numbers = []
-        # class_numbers_markings = []
 
         # Going through all output layers after feed forward pass
         for result in net_output:
@@ -160,9 +138,7 @@ def main(args):
                 confidence_current = scores[class_current]
 
                 # Eliminating weak predictions by minimum probability
-
-                if confidence_current > probability_minimum:
-
+                if confidence_current > confidence_threshold:
                     try:
                         # Scaling bounding box coordinates to the initial frame size
                         box_current = detected_objects[0:4] * np.array(
@@ -182,9 +158,7 @@ def main(args):
                         print(e)
 
         # Implementing non-maximum suppression of given bounding boxes
-        results = cv2.dnn.NMSBoxes(bounding_boxes, confidences, probability_minimum, threshold)
-        # results_markings = cv2.dnn.NMSBoxes(bounding_boxes_markings, bounding_boxes_markings,
-                                            # probability_minimum, threshold)
+        results = cv2.dnn.NMSBoxes(bounding_boxes, confidences, confidence_threshold, nms_threshold)
 
         # print("result: ", result)
         # Checking if there is any detected object been left
@@ -203,7 +177,7 @@ def main(args):
                 
                 else:
                     # Getting preprocessed blob with Traffic Sign of needed shape
-                    blob_ts = cv2.dnn.blobFromImage(frame_ts, 1 / 255.0, size=(32, 32), swapRB=True, crop=False)
+                    blob_ts = cv2.dnn.blobFromImage(frame_ts, scalefactor, size=(32, 32), swapRB=True, crop=False)
                     blob_ts[0] = blob_ts[0, :, :, :] - mean["mean_image_rgb"]
                     blob_ts = blob_ts.transpose(0, 2, 3, 1)
 
@@ -225,37 +199,6 @@ def main(args):
                     cv2.putText(frame, labels["SignName"][prediction], (x_min, y_min - 5),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_box_current, 2)
 
-        # # For markings
-        # for result in net_markings_output:
-        #     for detected_objects in result:
-        #         scores = detected_objects[5:]
-        #         class_current = np.argmax(scores)
-        #         confidence_current = scores[class_current]
-        #         if confidence_current > probability_minimum:
-        #             try:
-        #                 box_current = detected_objects[0:4] * np.array(
-        #                               [frame_width, frame_height, frame_width, frame_height])
-
-        #                 x_center, y_center, box_width, box_height = box_current
-        #                 x_min = int(x_center - box_width * 0.5)
-        #                 y_min = int(y_center - box_height * 0.5)
-
-        #                 bounding_boxes_markings.append([x_min, y_min, int(box_width), int(box_height)])
-        #                 confidences_markings.append(float(confidence_current))
-        #                 class_numbers_markings.append(class_current)
-                        
-        #             except Exception as e:
-        #                 print(e)
-
-        # print("results_markings: ", results_markings)
-        # if len(results_markings) > 0:
-        #     for i in results_markings.flatten():
-        #         x_min, y_min = bounding_boxes[i][0], bounding_boxes[i][1]
-        #         box_width, box_height = bounding_boxes[i][2], bounding_boxes[i][3]
-
-        #         cv2.rectangle(frame, (x_min, y_min),
-        #                       (x_min + box_width, y_min + box_height),
-        #                       colors[0].toList(), 2)
 
         # Write processed current frame to the file
         output_video.write(frame)
